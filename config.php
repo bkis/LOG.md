a684f57c0cfbc9650b3876d06a32ccb433594b1b
bkis <kiss@mailbox.org>
2019-11-22 21:19:52 +0100

    refactored constants

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
     define('LOGMD_THEME_NAME', 'default');
 
     define('LOGMD_HTML_TITLE', 'LOG.md');
-    define('LOGMD_PAGE_TITLE', 'LOG.md');
-    define('LOGMD_PAGE_SUBTITLE', 'A flat-file markdown blogging sytem so primitive it barely works');
+    define('LOGMD_HEADER_TITLE', 'LOG.md');
+    define('LOGMD_HEADER_SUBTITLE', 'A flat-file markdown blogging sytem so primitive it barely works');
 
     define('LOGMD_ROBOTS', 'noindex,nofollow');
     define('LOGMD_SAFE_MODE', 1);

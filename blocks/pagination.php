2dac28891a7afbb0e495d01ec70e1de1432f8561
bkis <kiss@mailbox.org>
2019-11-23 22:24:43 +0100

    pagination!!!

--- blocks/pagination.php
+++ blocks/pagination.php
@@ -1,18 +1,34 @@
 
 <div class="pagination">
-    <?php
-        if ($posts['PAGES'] > 1){
-            for ($i=1; $i <= $posts['PAGES']; $i++) { 
-                if ($i === $posts['PAGE']){
-    ?>
-        <span class="pagination-number"><?php echo $i ?></span>
-    <?php       } else { ?>
-        <span class="pagination-number">
-            <a href="?p=<?php echo $i ?>"><?php echo $i ?></a>
-        </span>
-    <?php       
-                }
-            }
-        }
-    ?>
+
+    <?php if ($posts['PAGES'] > 1){ ?>
+        
+        <!-- PREVIOUS -->
+        <a href="?p=<?php echo ($posts['PAGE'] == 1 ? $posts['PAGES'] : $posts['PAGE'] - 1) ?>">
+            <span class="pagination-link pagination-previous">
+                <?php echo LOGMD_PAGINATION_PREV ?>
+            </span>
+        </a>
+
+        <?php for ($i=1; $i <= $posts['PAGES']; $i++) { 
+                if ($i === $posts['PAGE']){ ?>
+        
+        <!-- CURRENT PAGE -->
+        <span class="pagination-link pagination-number"><?php echo $i ?></span>
+
+    <?php } else { ?>
+
+        <!-- OTHER PAGES -->
+        <a href="?p=<?php echo $i ?>"><span class="pagination-link pagination-number"><?php echo $i ?></span></a>
+
+    <?php } } ?>
+
+        <!-- NEXT -->
+        <a href="?p=<?php echo ($posts['PAGE'] == $posts['PAGES'] ? 1 : $posts['PAGE'] + 1) ?>">
+            <span class="pagination-link pagination-next">
+                <?php echo LOGMD_PAGINATION_NEXT ?>
+            </span>
+        </a>
+
+    <?php } ?>
 </div>
--- config.php
+++ config.php
@@ -1,15 +1,19 @@
 <?php
 
     define('LOGMD_POST_HEADER_DELIM', '++++');
-    define('LOGMD_THEME_NAME', 'sleeping-monkey'); // default themes: 'sleeping-monkey' and 'github-style'
+    define('LOGMD_THEME_NAME', 'github-style'); // default themes: 'sleeping-monkey' and 'github-style'
 
     define('LOGMD_HTML_TITLE', 'LOG.md');
     define('LOGMD_HEADER_TITLE', 'This is LOG.md');
     define('LOGMD_HEADER_SUBTITLE', 'A flat-file markdown blogging sytem so primitive it\'s a miracle it even works');
-    define('LOGMD_POSTS_PER_PAGE', 3);
     define('LOGMD_FOOTER_LINK', 1);
     define('LOGMD_POSTS_SORT_BY', 'TIME'); // can be 'TIME', 'TITLE' or whatever you are using in your posts headers, but it HAS TO BE UPPERCASED, here!
-
+    
+    // pagination
+    define('LOGMD_POSTS_PER_PAGE', 3);
+    define('LOGMD_PAGINATION_PREV', '&ltrif;');
+    define('LOGMD_PAGINATION_NEXT', '&rtrif;');
+    
     define('LOGMD_ROBOTS', 'noindex,nofollow');
     define('LOGMD_SAFE_MODE', 1);
 

2887f24d7a87b1a036fdca11cef395dc813aaa6a
bkis <kiss@mailbox.org>
2019-11-23 13:30:35 +0100

    advanced default themes

--- themes/github-style/_post.php
+++ themes/github-style/_post.php
@@ -1,4 +1,10 @@
 
+<div class="site-frame">
+    Read <strong>"<?php echo $post['TITLE'] ?>"</strong>
+    (published <?php echo $post['TIME'] ?>)
+    or go <a href="index.php">back</a>
+</div>
+
 <a id="content"></a>
 
 <article class="markdown-body">
--- themes/sleeping-monkey/_post.php
+++ themes/sleeping-monkey/_post.php
@@ -1,4 +1,9 @@
 
+<div class="site-frame">
+    &larr; <a href="index.php">back</a><br/>
+    <strong>&rarr; <?php echo $post['TITLE'] ?></strong> (<?php echo $post['TIME'] ?>)
+</div>
+
 <a id="content"></a>
 
 <article class="markdown-body">

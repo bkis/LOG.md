3d5ef8dd951c1f24e10edaf152607b0173431bc7
bkis <kiss@mailbox.org>
2019-11-23 15:59:27 +0100

    refactoring

--- func.php
+++ func.php
@@ -77,4 +77,20 @@
         );
     }
 
+    function getPostsData(){
+        // get all files and directories in posts dir
+        $posts = array_diff(scandir(LOGMD_POSTS_DIR), array('.', '..'));
+        // filter out everything that doesn't end on '.md' or '.MD'
+        $posts = array_values(preg_grep('/\.md$/i', $posts));
+        foreach ($posts as $i => $postFile){
+            // read post header
+            $posts[$i] = readPostHeader($postFile);
+        }
+        // sort posts by publishing time string
+        usort($posts, function($a, $b) {
+            return strcmp($a[LOGMD_POSTS_SORT_BY], $b[LOGMD_POSTS_SORT_BY]);
+        });
+        return $posts;
+    }
+
 ?>
--- index.php
+++ index.php
@@ -15,20 +15,8 @@
         $post = readPost(htmlspecialchars($_GET['post']) . '.md');
         $error = !$post;
     } else {
-        // get all files and directories in posts dir
-        $postFiles = array_diff(scandir(LOGMD_POSTS_DIR), array('.', '..'));
-        // filter out everything that doesn't end on '.md' or '.MD'
-        $postFiles = array_values(preg_grep('/\.md$/i', $postFiles));
-        // collect posts meta data
-        $posts = array();
-        foreach ($postFiles as $i => $postFile){
-            // read post header
-            $posts[$i] = readPostHeader($postFile);
-        }
-        // sort posts by publishing time string
-        usort($posts, function($a, $b) {
-            return strcmp($a[LOGMD_POSTS_SORT_BY], $b[LOGMD_POSTS_SORT_BY]);
-        });
+        // get all posts' meta data
+        $posts = getPostsData();
     }
 
 

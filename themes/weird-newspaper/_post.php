d7703bd8827f8d43fe04fffed0f1b297c70377e1
bkis <kiss@mailbox.org>
2019-11-25 18:00:13 +0100

    theme fix

--- themes/weird-newspaper/_post.php
+++ themes/weird-newspaper/_post.php
@@ -1,7 +1,9 @@
 
 <div class="position">
     <strong>&larrlp;</strong> <a href="javascript:history.back()">back</a><br/>
-    <strong>&rarrc;</strong> <em>"<?php echo $post['TITLE'] ?>"</em> <strong>&sdot;</strong> <i><?php echo $post['TIME'] ?>    </i>
+    <strong>&rarrc;</strong> <em>"<?php echo $post['TITLE'] ?>"</em>
+    &nbsp;&nbsp;&middot;&nbsp;&middot;&nbsp;&middot;&nbsp;
+    <i><?php echo $post['TIME'] ?>    </i>
 </div>
 
 <a id="content"></a>
